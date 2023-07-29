42c3baa2afc87e32f42440fe6dce39b302ebe330
Braio <mernickbryan@gmail.com>
2023-07-29 13:04:08 -0300

    ajuste de cores

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  // Definir o estado inicial para a cor do body
+  const [bodyColor, setBodyColor] = useState('');
+
+  // Função para atualizar a cor do body com base na cor do box em destaque
+  const handleBoxHover = (color) => {
+    setBodyColor(color);
+  };
 
   return (
-    <body>
-      <div class="container">
-        <div class="row">
-          <div class="col-5 box box_claro">
-
-          </div>
-          <div class="col-5 box box_escuro">
-
-          </div>
+    <body style={{ backgroundColor: bodyColor }}>
+      <div className="container">
+        <div className="row">
+          <div
+            className="col-5 box box_claro"
+            onMouseEnter={() => handleBoxHover('var(--branco-amarelado)')}
+            onMouseLeave={() => handleBoxHover('var(--azul-base)')}
+          ></div>
+          
+          <div
+            className="col-5 box box_escuro"
+            onMouseEnter={() => handleBoxHover('var(--preto-cinza)')}
+            onMouseLeave={() => handleBoxHover('var(--azul-base)')}
+          ></div>
         </div>
       </div>
     </body>
