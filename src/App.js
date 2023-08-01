d1f989a99d53278a9d2449c5360d3cda866ce441
Braio <mernickbryan@gmail.com>
2023-08-01 19:24:18 -0300

    Inserção de fotos e fontes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import java from './assets/imagens/java_icon.png';
+import tsIcon from './assets/imagens/ts_icon.png';
+
 import './style.css';
 
 export default function App() {
   // Definir o estado inicial para a cor do body
-  const [bodyColor, setBodyColor] = useState('');
+  const [bodyColor, setBodyColor] = useState('var(--azul-base)');
 
   // Função para atualizar a cor do body com base na cor do box em destaque
   const handleBoxHover = (color) => {
@@ -16,15 +19,21 @@ export default function App() {
         <div className="row">
           <div
             className="col-5 box box_claro"
-            onMouseEnter={() => handleBoxHover('var(--branco-amarelado)')}
-            onMouseLeave={() => handleBoxHover('var(--azul-base)')}
-          ></div>
-          
+            onMouseEnter={() => handleBoxHover('var(--azul-base)')}
+            onMouseLeave={() => handleBoxHover('var(--cinza)')}
+          >
+            <img src={tsIcon} alt="back"></img>
+            <h3>Front end</h3>
+          </div>
+
           <div
             className="col-5 box box_escuro"
             onMouseEnter={() => handleBoxHover('var(--preto-cinza)')}
-            onMouseLeave={() => handleBoxHover('var(--azul-base)')}
-          ></div>
+            onMouseLeave={() => handleBoxHover('var(--cinza)')}
+          >
+            <img src={java} alt="back"></img>
+            <h3>Back end</h3>
+          </div>
         </div>
       </div>
     </body>
