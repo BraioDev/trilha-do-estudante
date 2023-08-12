ef0957e78bc8c06dbfd9fa29d313733d05e871db
Braio <mernickbryan@gmail.com>
2023-08-12 13:27:08 -0300

    input e varias coisas

--- src/components/customJson/customJson.js
+++ src/components/customJson/customJson.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../../style.css'
 
 export default function CustomComponent() {
     const [jsonData, setJsonData] = useState({ front: [], back: [] });
+    const [searchKeyword, setSearchKeyword] = useState("");
 
     useEffect(() => {
         fetch(process.env.PUBLIC_URL + '/data.json')
@@ -14,12 +16,12 @@ export default function CustomComponent() {
             .catch(error => console.error('Erro ao buscar dados JSON:', error));
     }, []);
 
-    const scrollToSection = (event, targetId) => {
+    const scrollToSection = (event, targetId, offset = 0) => {
         event.preventDefault();
         const targetElement = document.getElementById(targetId);
 
         if (targetElement) {
-            targetElement.scrollIntoView({ behavior: 'smooth' });
+            targetElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
         }
     };
 
@@ -33,25 +35,53 @@ export default function CustomComponent() {
         <div className="container">
             <div className="conteudo">
                 <div className="row">
+                    <div className='topo'>
+                        <input
+                            className='espacamento input-pesquisa'
+                            type="text"
+                            value={searchKeyword}
+                            onChange={(e) => setSearchKeyword(e.target.value)}
+                            placeholder="Pesquisar"
+                        />
+                        {searchKeyword && (
+                            <button className="espacamento botao-limpar" onClick={() => setSearchKeyword("")}>
+                                X
+                            </button>
+                        )}
+                        <Link to="/" className='espacamento link-invisivel home'>
+                            <button className='button-dark'>Voltar</button>
+                        </Link>
+                    </div>
                     <div className='barra-lateral col-1-meio'>
                         <ul>
-                            {typeScriptData.topicos.map(topico => (
-                                <li key={topico.id}>
-                                    <a href={`#${topico.id}`} onClick={(e) => scrollToSection(e, topico.id)}>
-                                        {topico.nome}
-                                    </a>
-                                </li>
-                            ))}
+                            {typeScriptData.topicos.map(topico => {
+                                const lowerCaseTitulo = topico.Titulo.toLowerCase();
+                                if (lowerCaseTitulo.includes(searchKeyword.toLowerCase())) {
+                                    return (
+                                        <li key={topico.id}>
+                                            <a href={`#${topico.id}`} onClick={(e) => scrollToSection(e, topico.id)}>
+                                                {topico.Titulo}
+                                            </a>
+                                        </li>
+                                    );
+                                }
+                                return null;
+                            })}
                         </ul>
                     </div>
                     <div className="col-11">
-                        {typeScriptData.topicos.map(topico => (
-                            <div key={topico.id} id={topico.id} className="box-conteudo">
-                                <h2>{topico.nome}</h2>
-                                <img src={topico.conteudo} alt={topico.nome} />
-                                {/* Adicione mais conteúdo conforme necessário */}
-                            </div>
-                        ))}
+                        {typeScriptData.topicos.map(topico => {
+                            const lowerCaseTitulo = topico.Titulo.toLowerCase();
+                            if (lowerCaseTitulo.includes(searchKeyword.toLowerCase())) {
+                                return (
+                                    <div key={topico.id} id={topico.id} className="box-conteudo">
+                                        <h2>{topico.Titulo}</h2>
+                                        <img src={topico.conteudo} alt={topico.Titulo} />
+                                    </div>
+                                );
+                            }
+                            return null;
+                        })}
                     </div>
                 </div>
             </div>
