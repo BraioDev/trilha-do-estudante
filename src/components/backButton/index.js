cebf087c2072a5a7f576fbeb22aa9dc29dfa8861
Braio <mernickbryan@gmail.com>
2023-08-17 20:24:54 -0300

    adição de tooltip

--- src/components/backButton/index.js
+++ src/components/backButton/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { FaAngleLeft } from "react-icons/fa";
 import "../../style.css";
+import { Tooltip } from 'react-tippy';
+import 'react-tippy/dist/tippy.css';
 
 const BackButton = () => {
-  const handleBack = () => {
-    window.history.back();
-  };
+    const handleBack = () => {
+        window.history.back();
+    };
 
-  return (
-    <button className="botao button-dark espacamento" onClick={handleBack}>
-      <FaAngleLeft size={20} className="icon" />
-    </button>
-  );
+    return (
+        <Tooltip
+            title="voltar"
+            position="bottom"
+            trigger="mouseenter"
+            className="tool"
+        >
+            <button className="botao button-dark espacamento" onClick={handleBack}>
+                <FaAngleLeft size={20} className="icon" /> {/* Ícone de seta para trás */}
+            </button>
+        </Tooltip>
+    );
 };
 
 export default BackButton;
