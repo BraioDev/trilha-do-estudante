42ad6c7df69eead2b08a7c25ac306457a884c601
Braio <mernickbryan@gmail.com>
2023-11-07 19:33:17 -0300

    feat: adição de session e cokie

--- test/test_form.js
+++ test/test_form.js
@@ -28,14 +28,14 @@ describe("Função não salvar uma colaboração", () => {
 });
 
 describe("Função sobreescrever uma colaboração", () => {
-    it("Deve sobreescrever o salvamento se ja existir o dado com o mesmo nome", () => {
-      const dataToSave = {
-        nome: "TESTE",
-        texto: "TESTE",
-        assunto: "TESTE",
-      };
-  
-      const resultado = salvarColaboracao(dataToSave);
-      expect(resultado).to.equal(true);
-    });
+  it("Deve sobreescrever o salvamento se ja existir o dado com o mesmo nome", () => {
+    const dataToSave = {
+      nome: "TESTE",
+      texto: "TESTE",
+      assunto: "TESTE",
+    };
+
+    const resultado = salvarColaboracao(dataToSave);
+    expect(resultado).to.equal(true);
   });
+});
