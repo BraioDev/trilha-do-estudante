d8a8b1be2ee83bb422f4518cf832deaf78a97943
Braio <mernickbryan@gmail.com>
2023-11-12 16:38:39 -0300

    feat:  cookies

--- api/appe.js
+++ api/appe.js
@@ -59,9 +59,9 @@ const db = getFirestore(app);
 //Configurar o body-parser, para processar os dados do form
 appe.use(bodyParser.urlencoded({ extended: true }));
 
-appe.get("/", (req, res) => {
+/* appe.get("/", (req, res) => {
   res.render("index");
-});
+}); */
 
 appe.post("/salvar", async (req, res) => {
   try {
@@ -120,27 +120,30 @@ appe.get("/home", (req, res) => {
   }
 });
 
-appe.get("/dados-do-servidor", (req, res) => {
-  const data = {
-    nodeVersion: process.versions.node,
-  };
-  res.json(data);
-});
-
 /*Session e cookie*/
 //Rota para exibir produtos
-appe.get("/produtos", (req, res) => {
+appe.get("/", (req, res) => {
+  res.cookie("usuario_logado", {
+    nome: "Jão",
+    email: "bryan@example.com",
+    url: "https://a.imagem.app/oqijzN.png",
+  });
   res.send(`
-  <h1>Lista de produtos</h1>
-  <ul>
+  <div style="font-family: 'Cera Round Pro', sans-serif; text-transform: uppercase;">
+  <h1 style="text-align: center; margin-top: 80px;">Lista de produtos</h1>
+  <ul style="text-align: center; margin-left: 530px; margin-top:50px;">
   ${produtos
     .map(
       (produto) =>
-        `<li>${produto.nome} - ${produto.preco} <a href="/adicionar/${produto.id}"> adicionar ao carrinho </a></li>`
+        `<li style="list-style-type: none; text-align: left; margin-top: 20px; text-transform: uppercase;">${produto.nome} - ${produto.preco} <a href="/adicionar/${produto.id}" style="display: inline-block; background-color: #007bff; color: #fff; padding: 10px 20px; text-align: center; text-decoration: none; border: none; border-radius: 5px;">Adicionar</a></li>`
     )
     .join("")}
-  </ul>
-  <a href="/carrinho"> Ver carrinho</a>
+</ul>
+<div style="display: flex; justify-content: center;  margin-top:50px;">
+<a href="/carrinho" style="display: inline-block; background-color: #007bff; color: #fff; padding: 10px 20px; text-align: center; text-decoration: none; border: none; border-radius: 5px; margin-right: 10px;">Ver carrinho</a>
+<a href="/perfil" style="display: inline-block; background-color: #007bff; color: #fff; padding: 10px 20px; text-align: center; text-decoration: none; border: none; border-radius: 5px;"> Seu perfil </a>
+</div>
+</div>
   `);
 });
 
@@ -155,7 +158,7 @@ appe.get("/adicionar/:id", (req, res) => {
     req.session.carrinho.push(produto);
   }
 
-  res.redirect("/produtos");
+  res.redirect("/");
 });
 
 appe.get("/carrinho", (req, res) => {
@@ -165,15 +168,27 @@ appe.get("/carrinho", (req, res) => {
   res.send(`
   <h1>Carrinho de compras</h1>
   <ul>
-  ${carrinho.map(
-      (produto) =>
-        `<li>${produto.nome} - ${produto.preco}<li>`
-    )
+  ${carrinho
+    .map((produto) => `<li>${produto.nome} - ${produto.preco}<li>`)
     .join("")}
   </ul>
 
   <p>Total: ${total}</p>
-  <a href="/produtos">Continuar comprando<a/>
+  <a href="/">Continuar comprando<a/>
+  `);
+});
+
+appe.get("/perfil", (req, res) => {
+  const usuarioLogado = req.cookies.usuario_logado;
+  res.send(`
+  <h1>Seu perfil</h1>
+  <ul>
+  <img src=${usuarioLogado.url} alt="foto" border="0" />
+  <li>Nome: ${usuarioLogado.nome}</li>
+  <li>Email: ${usuarioLogado.email}</li>
+  </ul>
+  
+  <a href="/">Voltar as compras<a/>
   `);
 });
 
